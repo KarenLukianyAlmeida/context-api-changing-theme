e599d83b80fa2a5f92c9af479e511887c0d9b8c5
Karen Almeida <karenalmeida.lukiany@gmail.com>
2024-02-18 15:38:35 +0000

    Ajusta css

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import './App.css'
 import ThemeContext from './context/ThemeContext'
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Image from './components/Image';
 import { useState } from 'react';
+import './App.css'
 
 export default function App() {
   const [pageTheme, setPageTheme] = useState<'dark' | 'light'>('dark');
@@ -14,7 +14,7 @@ export default function App() {
 
   return (
     <ThemeContext.Provider value={{ theme: pageTheme, toogleTheme}}>
-      <div>
+      <div className={`app ${pageTheme}`}>
         <Header />
         <Image /> 
         <Footer />
--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,7 +1,7 @@
 export default function Image() {
   return (
     <div>
-      <img src="../../public/trybe.svg" alt="trybe logo" />
+      <img src="/trybe.svg" alt="trybe logo" />
     </div>
   );
 }
